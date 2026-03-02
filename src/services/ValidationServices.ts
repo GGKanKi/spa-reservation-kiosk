226db4bbc00dae22e2fc5c6cb2b6d892c5c4b522
GGKanKi <bheaded.65@gmail.com>
2026-03-02 20:51:10 +0800

    Updated Validation For Signing Up

--- src/services/ValidationServices.ts
+++ src/services/ValidationServices.ts
@@ -10,7 +10,7 @@ export const UserSchema = z.object({
     middleName: z.string().min(2, "Name Must 2 Characters Long."),
     lastName: z.string().min(2, "Name Must 2 Characters Long."),
     emailAddress: z.string().email("Invalid Email"),
-    phoneNum: z.string("Invalid Number"),
+    phoneNum: z.string().min(10, "Phone number must be at least 10 digits"),
     username: z.string().min(8, "Username must be 8 Characters Long."),
     password: z.string().min(8, "Password must be 8 Characters Long."),
     created_at: z.string().datetime(),
@@ -18,12 +18,28 @@ export const UserSchema = z.object({
 
 });
 
+// Extension For SignUpSchema
+
+export const SignUpSchema = UserSchema.omit({
+    created_at: true,
+    userRole: true,
+}).extend({
+    confirmPassword: z.string().min(8, "Confirm Password must be 8 Characters Long.")
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+
+});
+
+
+export type SignUpFormData = z.infer<typeof SignUpSchema>;
+
 export const ServiceSchema = z.object({
 
-    serviceName: z.string().min(5, "Service Name Must Be 5 Characters Longs."),
+    serviceName: z.string().min(5, "Service Name Must Be 5 Characters Long."),
     category: z.enum(['massage', 'spa', 'facial', 'body_wrap', 'nail_care', 'aromatherapy', 'bundle']),
-    price: z.number(),
-    description: z.string().min(10, 'Desciprtion Should Be A Sentence'),
+    price: z.number().positive('Price must be greater than 0'),
+    description: z.string().min(10, 'Description Should Be A Sentence'),
     createdAt: z.string().datetime(),
 });
 
@@ -33,7 +49,7 @@ export const OrderSchema = z.object({
     serviceId: z.string(), // ID Connected To Services
     clientId:  z.string(), // ID Connected to Client Who Ordered
     staffId: z.string(), // ID where User.role = Staff
-    roomId: z.string(), //Room Id onyl for avaialble rooms
+    roomId: z.string(), // Room Id only for available rooms
     orderStatus: z.enum(['pending', 'cleaning', 'close', 'reserve', 'maintenance']),
     createdAt: z.string().datetime(),
 
@@ -54,8 +70,8 @@ export const RoomSchema = z.object({
 
 export const ReservationSchema = z.object({
 
-    reservationName: z.string().min(5, 'Reservationn Name Should be 5 Characters Long.'),
-    clientId: z.string(), // UsedID where UserRole = member/VIP
+    reservationName: z.string().min(5, 'Reservation Name Should be 5 Characters Long.'),
+    clientId: z.string(), // UserID where UserRole = member/VIP
     orderId: z.string(), // Client Order
     roomId: z.string(), // Room ID for avaialble Room
     reservationTime: z.string().datetime(),
