093b208c6d86b57a1afac29bf6c1ca1649ead4e5
GGKanKi <bheaded.65@gmail.com>
2026-04-04 23:49:16 +0800

    fixed: Date Issue - Timestamptz mismatch

--- src/services/OrderServices.ts
+++ src/services/OrderServices.ts
@@ -51,7 +51,6 @@ export const Orders = {
         return {data, error: null}
     },
 
-
     async selectOrder(orderId: string) {
         const {data, error} = await supabase
             .from('Order')
@@ -94,6 +93,9 @@ export const Orders = {
             staff_id: validation.data.staffId,
             room_id: validation.data.roomId,
             order_status: 'pending',
+            reservation_date: validation.data.reservationDate,
+            start_time: validation.data.startTime,
+            end_time: validation.data.endTime
         }
         ])
         .select()
